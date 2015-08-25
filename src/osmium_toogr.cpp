6be289b135f4677fcd6fe678af56c67aaf6f5804
Jochen Topf <jochen@topf.org>
2015-08-25 11:44:27 +0200

    Use gdalcpp wrapper classes new in libosmium.

--- src/osmium_toogr.cpp
+++ src/osmium_toogr.cpp
@@ -8,8 +8,7 @@
 #include <iostream>
 #include <getopt.h>
 
-#include <ogr_api.h>
-#include <ogrsf_frmts.h>
+#include <gdalcpp.hpp>
 
 #include <osmium/index/map/all.hpp>
 #include <osmium/handler/node_locations_for_ways.hpp>
@@ -26,107 +25,31 @@ typedef osmium::handler::NodeLocationsForWays<index_pos_type, index_neg_type> lo
 
 class MyOGRHandler : public osmium::handler::Handler {
 
-    OGRDataSource* m_data_source;
-    OGRLayer* m_layer_point;
-    OGRLayer* m_layer_linestring;
+    gdalcpp::Layer m_layer_point;
+    gdalcpp::Layer m_layer_linestring;
 
     osmium::geom::OGRFactory<> m_factory;
 
 public:
 
-    MyOGRHandler(const std::string& driver_name, const std::string& filename) {
+    MyOGRHandler(gdalcpp::Dataset& dataset) :
+        m_layer_point(dataset, "postboxes", wkbPoint),
+        m_layer_linestring(dataset, "roads", wkbLineString) {
 
-        OGRRegisterAll();
+        m_layer_point.add_field("id", OFTReal, 10);
+        m_layer_point.add_field("operator", OFTString, 30);
 
-        OGRSFDriver* driver = OGRSFDriverRegistrar::GetRegistrar()->GetDriverByName(driver_name.c_str());
-        if (!driver) {
-            std::cerr << driver_name << " driver not available.\n";
-            exit(1);
-        }
-
-        CPLSetConfigOption("OGR_SQLITE_SYNCHRONOUS", "FALSE");
-        const char* options[] = { "SPATIALITE=TRUE", nullptr };
-        m_data_source = driver->CreateDataSource(filename.c_str(), const_cast<char**>(options));
-        if (!m_data_source) {
-            std::cerr << "Creation of output file failed.\n";
-            exit(1);
-        }
-
-        OGRSpatialReference sparef;
-        sparef.SetWellKnownGeogCS("WGS84");
-        m_layer_point = m_data_source->CreateLayer("postboxes", &sparef, wkbPoint, nullptr);
-        if (!m_layer_point) {
-            std::cerr << "Layer creation failed.\n";
-            exit(1);
-        }
-
-        OGRFieldDefn layer_point_field_id("id", OFTReal);
-        layer_point_field_id.SetWidth(10);
-
-        if (m_layer_point->CreateField(&layer_point_field_id) != OGRERR_NONE) {
-            std::cerr << "Creating id field failed.\n";
-            exit(1);
-        }
-
-        OGRFieldDefn layer_point_field_operator("operator", OFTString);
-        layer_point_field_operator.SetWidth(30);
-
-        if (m_layer_point->CreateField(&layer_point_field_operator) != OGRERR_NONE) {
-            std::cerr << "Creating operator field failed.\n";
-            exit(1);
-        }
-
-        /* Transactions might make things faster, then again they might not.
-           Feel free to experiment and benchmark and report back. */
-        m_layer_point->StartTransaction();
-
-        m_layer_linestring = m_data_source->CreateLayer("roads", &sparef, wkbLineString, nullptr);
-        if (!m_layer_linestring) {
-            std::cerr << "Layer creation failed.\n";
-            exit(1);
-        }
-
-        OGRFieldDefn layer_linestring_field_id("id", OFTReal);
-        layer_linestring_field_id.SetWidth(10);
-
-        if (m_layer_linestring->CreateField(&layer_linestring_field_id) != OGRERR_NONE) {
-            std::cerr << "Creating id field failed.\n";
-            exit(1);
-        }
-
-        OGRFieldDefn layer_linestring_field_type("type", OFTString);
-        layer_linestring_field_type.SetWidth(30);
-
-        if (m_layer_linestring->CreateField(&layer_linestring_field_type) != OGRERR_NONE) {
-            std::cerr << "Creating type field failed.\n";
-            exit(1);
-        }
-
-        m_layer_linestring->StartTransaction();
-    }
-
-    ~MyOGRHandler() {
-        m_layer_linestring->CommitTransaction();
-        m_layer_point->CommitTransaction();
-        OGRDataSource::DestroyDataSource(m_data_source);
-        OGRCleanupAll();
+        m_layer_linestring.add_field("id", OFTReal, 10);
+        m_layer_linestring.add_field("type", OFTString, 30);
     }
 
     void node(const osmium::Node& node) {
         const char* amenity = node.tags().get_value_by_key("amenity");
         if (amenity && !strcmp(amenity, "post_box")) {
-            OGRFeature* feature = OGRFeature::CreateFeature(m_layer_point->GetLayerDefn());
-            std::unique_ptr<OGRPoint> ogr_point = m_factory.create_point(node);
-            feature->SetGeometry(ogr_point.get());
-            feature->SetField("id", static_cast<double>(node.id()));
-            feature->SetField("operator", node.tags().get_value_by_key("operator"));
-
-            if (m_layer_point->CreateFeature(feature) != OGRERR_NONE) {
-                std::cerr << "Failed to create feature.\n";
-                exit(1);
-            }
-
-            OGRFeature::DestroyFeature(feature);
+            gdalcpp::Feature feature(m_layer_point, m_factory.create_point(node));
+            feature.set_field("id", static_cast<double>(node.id()));
+            feature.set_field("operator", node.tags().get_value_by_key("operator"));
+            feature.add_to_layer();
         }
     }
 
@@ -134,18 +57,10 @@ public:
         const char* highway = way.tags().get_value_by_key("highway");
         if (highway) {
             try {
-                std::unique_ptr<OGRLineString> ogr_linestring = m_factory.create_linestring(way);
-                OGRFeature* feature = OGRFeature::CreateFeature(m_layer_linestring->GetLayerDefn());
-                feature->SetGeometry(ogr_linestring.get());
-                feature->SetField("id", static_cast<double>(way.id()));
-                feature->SetField("type", highway);
-
-                if (m_layer_linestring->CreateFeature(feature) != OGRERR_NONE) {
-                    std::cerr << "Failed to create feature.\n";
-                    exit(1);
-                }
-
-                OGRFeature::DestroyFeature(feature);
+                gdalcpp::Feature feature(m_layer_linestring, m_factory.create_linestring(way));
+                feature.set_field("id", static_cast<double>(way.id()));
+                feature.set_field("type", highway);
+                feature.add_to_layer();
             } catch (osmium::geometry_error&) {
                 std::cerr << "Ignoring illegal geometry for way " << way.id() << ".\n";
             }
@@ -230,7 +145,9 @@ int main(int argc, char* argv[]) {
     location_handler_type location_handler(*index_pos, index_neg);
     location_handler.ignore_errors();
 
-    MyOGRHandler ogr_handler(output_format, output_filename);
+    CPLSetConfigOption("OGR_SQLITE_SYNCHRONOUS", "FALSE");
+    gdalcpp::Dataset dataset(output_format, output_filename, "", { "SPATIALITE=TRUE" });
+    MyOGRHandler ogr_handler(dataset);
 
     osmium::apply(reader, location_handler, ogr_handler);
     reader.close();
